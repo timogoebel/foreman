c047ccc7980a1875be9a35018378c405ca502a80
Timo Goebel <mail@timogoebel.name>
2017-03-20 17:04:10 +0100

    remove old volumes view, pass controller_types to react

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -933,7 +933,7 @@ class HostsController < ApplicationController
       deep_symbolize_keys
     volumes = {}
     scsi_and_vol[:volumes].each_with_index do |vol, index|
-      volumes["#{index}"] = vol
+      volumes[index.to_s] = vol
     end
 
     host_params["compute_attributes"]["scsi_controllers"] = scsi_and_vol[:scsi_controllers]
